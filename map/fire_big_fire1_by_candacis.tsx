cc9be7fcc067849aa5a85729b092f2b7370b77e6
herrsei123 <78570906+herrsei123@users.noreply.github.com>
2021-04-13 23:55:25 +0200

    Add files via upload

---
+++ map/fire_big_fire1_by_candacis.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="fire_big_fire1_by_candacis" tilewidth="32" tileheight="32" tilecount="48" columns="6">
+ <image source="../maps_ressourcen/tiles/fire_big_fire1_by_candacis.png" width="192" height="256"/>
+</tileset>
---
+++ maps_ressourcen/tiles/trees_nature_tileset_bosque_expansion_rtp___rpg_maker_xp_by_mataraelfay_d5dn3fn.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="trees_nature_tileset_bosque_expansion_rtp___rpg_maker_xp_by_mataraelfay_d5dn3fn" tilewidth="32" tileheight="32" tilecount="1496" columns="8">
+ <image source="trees_nature_tileset_bosque_expansion_rtp___rpg_maker_xp_by_mataraelfay_d5dn3fn.png" width="256" height="6000"/>
+</tileset>
